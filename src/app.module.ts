56f7a80d0463be4cecdae934ac3676a2007e02f0
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-02-22 14:04:07 +0200

    added tokenService to Providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TokenController } from './token.controller';
+import { TokenService } from './token.service';
 
 @Module({
   imports: [],
   controllers: [AppController, TokenController],
-  providers: [AppService],
+  providers: [AppService, TokenService],
 })
 export class AppModule {}
